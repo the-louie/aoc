a2259335f4b8552bfb86d2b86ee59d6233832e42
The Louie <github@louie.se>
2020-12-03 10:34:53 +0100

    Minor fixes to day 1

--- 2020/1/both.js
+++ 2020/1/both.js
@@ -6,13 +6,13 @@ const testinput = `1721
 675
 1456`
 
-const testdata = testinput.split('\n').filter(f => f != '')
+const testdata = testinput.split('\n').filter(f => f != '').map(d => parseInt(d))
 
 const part1 = (data) => {
     for (let i = 0; i<data.length-1; i++) {
         for (let j=i+1; j<data.length; j++) {
-            if (parseInt(data[i]) + parseInt(data[j]) === 2020) {
-                return parseInt(data[i]) * parseInt(data[j])
+            if (data[i] + data[j] === 2020) {
+                return data[i] * data[j]
             }
         }
     }
@@ -23,8 +23,8 @@ const part2 = (data) => {
     for (let i = 0; i<data.length-2; i++) {
         for (let j=i+1; j<data.length-1; j++) {
             for (let k=j+1; k<data.length; k++) {
-                if (parseInt(data[i]) + parseInt(data[j]) + parseInt(data[k]) === 2020) {
-                    return parseInt(data[i]) * parseInt(data[j] * parseInt(data[k]))
+                if (data[i] + data[j] + data[k] === 2020) {
+                    return data[i] * data[j] * data[k]
                 }
             }
         }
@@ -32,7 +32,7 @@ const part2 = (data) => {
     return '*ERR*'
 }
 
-const data = fs.readFileSync('./input.txt').toString().split('\n').filter(f => f != '')
+const data = fs.readFileSync('./input.txt').toString().split('\n').filter(f => f != '').map(d => parseInt(d))
 
 const r1 = part1(data)
 console.log('part1', r1)
