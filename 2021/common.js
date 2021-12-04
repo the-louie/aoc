4c1dede4ab86e05a97fbb4f99887eba28ff09771
The Louie <github@louie.se>
2021-12-04 20:28:12 +0100

    added more common funcs

--- 2021/common.js
+++ 2021/common.js
@@ -2,8 +2,13 @@ const bufToArr = (buf) => buf.toString().replace(/\r/g, '').split('\n').filter(r
 const bufToIntArr = (buf) => bufToArr(buf).map(i => Number(i))
 const bufToStrArr = (buf) => bufToArr(buf).map(i => String(i))
 
+const createArr = (l, v) => Array.apply(null, Array(l)).map(() => v)
+const createSeqArr = (l) => Array.apply(null, Array(l)).map((_n, i) => i)
+
 module.exports = {
   bufToArr,
   bufToIntArr,
-  bufToStrArr
+  bufToStrArr,
+  createArr,
+  createSeqArr
 }
