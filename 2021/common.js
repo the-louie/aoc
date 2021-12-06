95791fe5852c5a3e7a4036cc4a836c836f98a905
The Louie <github@louie.se>
2021-12-06 17:43:05 +0100

    added arrsum to common

--- 2021/common.js
+++ 2021/common.js
@@ -5,10 +5,13 @@ const bufToStrArr = (buf) => bufToArr(buf).map(i => String(i))
 const createArr = (l, v = 0) => Array.apply(null, Array(l)).map(() => v)
 const createSeqArr = (l, s = 0) => Array.apply(null, Array(l)).map((_n, i) => i + s)
 
+const arrSum = (arr) => arr.reduce((acc, curr) => acc + curr, 0)
+
 module.exports = {
   bufToArr,
   bufToIntArr,
   bufToStrArr,
   createArr,
-  createSeqArr
+  createSeqArr,
+  arrSum
 }
